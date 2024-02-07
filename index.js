80bb378af8a65b6bd72584d33d43a87da2c45b3c
Charlotte Stone <charlie.stone649@gmail.com>
2024-02-07 18:50:19 +0000

    Add various changes to each file in testing jQuery

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
 $(document).ready(function(){
-    $("h1").css("color", "red");
+    $("h1").addClass("big-title");
+    $("h1").text("This is some changed text with jQuery");
+    $("h1").removeClass("big-title");
+    $("button").html("<em>I'm reborn!</em>");
+    $("img").attr("src", "https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png");
+
+    $("h1").click(function(){
+        $("h1").css("color", "purple");
+    })
+    $("button").click(function(){
+        $("h1").css("color", "red");
+    })
+    $("input").keypress(function(event){
+        $("h1").text(event.key);
+    })
+
+    $("img").on("mouseover", function(){
+        $("img").attr("src", "http://t3.gstatic.com/images?q=tbn:ANd9GcSoorKS-3CxgdJFB3QNWhu_L7DTYVdgGMybnW1PUwgcoqmRLmO8N8D7yfBGzC_BZuLlBkB_IVLnv2WWgus4vlNRn3su7MRHDChgWqfXadI");
+    })
 });
